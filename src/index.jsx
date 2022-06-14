fa3fca64911b4529e44e6519c46fa88bb35d91d7
Mohamed Aashik N <aashikm015@gmail.com>
2022-06-15 00:09:13 +0530

    First Hello World Like Codes

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+export function App({title})
+{
+    return (
+        <h1 style={{textAlign:"center"}}>Hello ! am {title}</h1>
+    )
+}
+export function Appy({title})
+{
+    return (
+        <h1>Hello I am appy {title}</h1>
+    )
+}
+
+function Deff({title })
+{
+    return (
+        <h1>Hello i am deff {title}</h1>
+    )
+ 
+}
+export default Deff;
